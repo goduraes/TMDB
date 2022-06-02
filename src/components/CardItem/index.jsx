3b17e8f6f46dcabfb8ac24748c2ff7521cfcdbd3
Gabriel Durães <gabriel.duraes@consuti.com.br>
2022-06-02 09:26:13 -0300

    percentagem ratio in item

--- src/components/CardItem/index.jsx
+++ src/components/CardItem/index.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import PropTypes, { string } from 'prop-types';
+import styled from 'styled-components';
 
 import './index.css';
+
+const Ratio = styled.div`
+  --ratio: ${props => props.percentage ?  props.percentage :  0};
+`;
 
 const CardItem = ({ item }) => {
   const {
@@ -20,15 +25,25 @@ const CardItem = ({ item }) => {
     ? `https://image.tmdb.org/t/p/w200/${urlImg}`
     : '/200x300.png';
 
+  const percentageRatio =
+    vote_average !== 10
+      ? `.${vote_average.toFixed(1).replace('.', '')}`
+      : vote_average;
+
   return (
-    <div key={id} id="cardItem" className="relative rounded z-10">
-      <img className="rounded md:h-auto" src={urlImg} alt="" />
+    <div key={id} id="cardItem" className="relative rounded-lg z-10">
+      <div className="img-item relative">
+        <img className="rounded-lg md:h-auto" src={urlImg} alt="" />
+        <div className="voteAverage font-bold text-white">
+          <span className="absolute z-10 text-sm">{vote_average}</span>
+          <Ratio className="ratio" percentage={percentageRatio} />
+        </div>
+      </div>
       <div className="flex flex-col justify-between max-w-full">
-        <span className="font-bold text-gray-900 text-center mt-3 px-2">
+        <span className="font-bold text-gray-900 text-center mt-4 px-2">
           {titleItem}
         </span>
       </div>
-      <div className="voteAverage font-bold text-white">{vote_average}</div>
     </div>
   );
 };
